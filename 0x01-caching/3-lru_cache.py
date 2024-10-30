6213c43454b53c2eea9e1285f4418c48ac8533b1
Stringpappy <jejeodunayo70@gmail.com>
2024-10-30 14:26:35 -0700

    cache task

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""
+cching module"""
 from BaseCaching import BaseCaching
 
 class LRUCache(BaseCaching):
@@ -8,7 +10,7 @@ class LRUCache(BaseCaching):
         """Initialize the class."""
         super().__init__()
         self.cache_data = {}
-        self.order = []  # Keep track of the order of access
+        self.order = []
 
     def put(self, key, item):
         """Add an item to the cache."""
@@ -16,27 +18,23 @@ class LRUCache(BaseCaching):
             return
 
         if key in self.cache_data:
-            # Update the item and mark it as recently used
             self.cache_data[key] = item
-            self.order.remove(key)  # Remove the key from its current position
-            self.order.append(key)   # Re-add it to mark it as most recently used
+            self.order.remove(key)
+            self.order.append(key)
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Remove the least recently used item (the first in the order list)
                 lru_key = self.order.pop(0)
-                del self.cache_data[lru_key]  # Remove it from the cache
+                del self.cache_data[lru_key]
                 print(f"DISCARD: {lru_key}")
 
-            # Add the new key-value pair
             self.cache_data[key] = item
-            self.order.append(key)  # Add the new key to the order list
+            self.order.append(key)
 
     def get(self, key):
         """Get an item from the cache."""
         if key is None or key not in self.cache_data:
             return None
 
-        # Mark this key as recently used by moving it to the end
         self.order.remove(key)
         self.order.append(key)
 
