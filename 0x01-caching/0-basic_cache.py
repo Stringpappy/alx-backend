0a9aea5681f9e872f9f9f359a5065871a219cd34
Stringpappy <jejeodunayo70@gmail.com>
2024-10-30 14:15:50 -0700

    cache task

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-"""module that Create a class BasicCache that inherits
-from BaseCaching and is a caching system"""
-from BaseCaching import BaseCaching
+"""
+caching module.
+"""
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """BasicCache class that inherits from BaseCaching."""
-
-    def __init__(self):
-        """initialise the class """
-        super().__init__()
-        self.cache_data = {}
-
+    """
+    retrieving items from a dictionary.
+    """
     def put(self, key, item):
-        """Add an item to the cache."""
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        """Get an item from the cache."""
+        """Retrieves an item by key.
+        """
         return self.cache_data.get(key, None)
